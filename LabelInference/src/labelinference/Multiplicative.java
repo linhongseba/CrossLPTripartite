782fa5ed25bc1708234424e3f6b195a9911a347f
Hermes777 <13307130309@fudan.edu.cn>
2015-07-15 21:31:36 -0700

    AddClassMultiplicative

--- LabelInference/src/labelinference/Multiplicative.java
+++ LabelInference/src/labelinference/Multiplicative.java
@@ -1,31 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package labelinference;
 
 import labelinference.exceptions.ColumnOutOfRangeException;
 import labelinference.exceptions.RowOutOfRangeException;
 
-/**
- *
- * @author Tangyiqi
- */
 public class Multiplicative {
 	Matrix Y0;
 	Matrix Y;
 	Matrix Gab,Gbc,Gac;
-	Multiplicative(Matrix M,Graph Yl,int na,int nb,int nc) throws ColumnOutOfRangeException, RowOutOfRangeException
+	Matrix Sa,Sb,Sc;
+	int na,nb,nc;
+	//M is the matrix transfered from the tripartite graph. Yl is the ground matrix. 
+	Multiplicative(Matrix M,Matrix Yl,int Na,int Nb,int Nc) throws ColumnOutOfRangeException, RowOutOfRangeException
 	{
-		//Gab=M.subMatrix(0,na-1,na,na+nb-1);
-		//Gbc=M.subMatrix(na,na+nb-1,na+nb,na+nb+nc-1);
-		//Gac=M.subMatrix(0,na-1,na+nb,na+nb+nc-1);
-		
-		
+		Y0=new NaiveMatrix(Na+Nb+Nc,2);
+		Sa=new NaiveMatrix(Na,Na+Nb+Nc);
+		na=Na;nb=Nb;nc=Nc;
+		Gab=M.subMatrix(0,na-1,na,na+nb-1);
+		Gbc=M.subMatrix(na,na+nb-1,na+nb,na+nb+nc-1);
+		Gac=M.subMatrix(0,na-1,na+nb,na+nb+nc-1);
+		for(int i=0;i<na;i++)
+			for(int j=0;j<2;j++)
+				if(Yl.get(i,j)>0)
+				{
+					Y0.set(i, j, 1);
+					Sa.set(i, i, 1);
+				}
+		for(int i=0;i<nb;i++)
+			for(int j=0;j<2;j++)
+				if(Yl.get(i,j)>0)
+				{
+					Y0.set(na+i, j, 1);
+					Sb.set(i, i, 1);
+				}
+		for(int i=0;i<nc;i++)
+			for(int j=0;j<2;j++)
+				if(Yl.get(i,j)>0)
+				{
+					Y0.set(na+nb+i, j, 1);
+					Sc.set(i, i, 1);
+				}
+		Y=Y0;
 	}
-	public void update()
+	public void update() throws ColumnOutOfRangeException, RowOutOfRangeException 
 	{
+		Matrix Ya=Y.subMatrix(0,na-1,0,2);
+		Matrix Yb=Y.subMatrix(na,na+nb-1,0,2);
+		Matrix Yc=Y.subMatrix(na+nb,na+nb+nc-1,0,2);
+		
 	}
 	public boolean converge()
 	{
