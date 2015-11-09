0319e74d58f8cbc90e44658949e8d489ef216301
Hermes777 <13307130309@fudan.edu.cn>
2015-11-08 18:48:46 -0800

    result updated

--- LabelInference/experiment/experimental_scripts/converter.py
+++ LabelInference/experiment/experimental_scripts/converter.py
@@ -1,7 +1,7 @@
 #
 # Running on python3
 #
-import xlwt
+import xlwt3 as xlwt
 import math
 
 def _isdigit(ch):
@@ -41,23 +41,19 @@ def find_xls(filename,_type):
     obj=''
     time=''
     obj_list=[]
-    if _type==2:
-        while True:
-            line = f.readline()
-            if line.find('Recompute2')>=0:
-                break;
+
     label=0
     while True:
         line = f.readline()
         if not line:
             break
-        if line.find('ObjValue')>=0 and line.find('0.000000')<0 and _type==2:
-            obj_list.append(float(line[11:]))
+        if line.find('ObjValue ')>=0 and line.find('0.000000')<0 and _type==2:
+            obj_list.append(float(line[10:]))
         if line.find('Processed in ')>=0 and line.find('total')>=0 and _type==3:
             return fil_line(line)
         if _type==2 and line.find('confidence')>=0:
             break;
-        if line.find('Global')>=0:
+        if line.find('Old Accuracy')>=0:
             label=1
         if line.find('Accuracy ')>=0 and _type==1 and label==1:
             return fil_line(line[line.find('(C)'):])
@@ -68,12 +64,31 @@ def find_xls(filename,_type):
 xls=xlwt.Workbook()
 
 #================================================================================
-sheet = xls.add_sheet('accuracy',cell_overwrite_ok=True)
+sheet = xls.add_sheet('Old_accuracy(0.000001)',cell_overwrite_ok=True)
 
-graph=['graph-30','graph-37','graph_imdb_10M','graph_paper'];
-algorithm=['MRG','ARG','MRO','ARO'];
+graph=['graph-30','graph-37','graph_imdb','graph_paper']
+algorithm=['MRG','ARG','GRF','MRO','ARO'];
 
-for i in range(0,4):
+for i in range(0,5):
+    sheet.write(0,1+i,algorithm[i])
+
+x=0
+for g in graph:
+    x+=1
+    sheet.write(x,0,g)
+    y=0
+    for a in algorithm:
+        y+=1
+        tot=0;
+        filename=g+'_05_'+a+'_0_0_0.000001_5.txt';
+        sheet.write(x,y,find_xls(filename,1))
+#================================================================================#================================================================================
+sheet = xls.add_sheet('Old_accuracy(-1)',cell_overwrite_ok=True)
+
+graph=['graph-30','graph-37','graph_imdb','graph_paper']
+algorithm=['MRG','ARG','GRF','MRO','ARO'];
+
+for i in range(0,5):
     sheet.write(0,1+i,algorithm[i])
 
 x=0
@@ -89,14 +104,14 @@ for g in graph:
 #================================================================================
 #================================================================================
 
-graph=['graph-30','graph-37','graph_imdb_10M','graph_paper'];
-algorithm=['MRG','ARG','MRO','ARO'];
+graph=['graph-30','graph-37','graph_imdb','graph_paper']
+algorithm=['MRG','ARG','GRF','MRO','ARO'];
 
 
 for g in graph:
-    sheet = xls.add_sheet('converge-recompute2'+g,cell_overwrite_ok=True)
+    sheet = xls.add_sheet('converge'+g,cell_overwrite_ok=True)
     sheet.write(0,0,'#iteration')
-    for i in range(0,4):
+    for i in range(0,5):
         sheet.write(0,1+i,algorithm[i])
 
     y=0
@@ -118,13 +133,34 @@ for g in graph:
 
 #================================================================================
 #================================================================================
-sheet = xls.add_sheet('running_time',cell_overwrite_ok=True)
+sheet = xls.add_sheet('running_time(0.000001)',cell_overwrite_ok=True)
 
-graph=['graph-30','graph-37','graph_imdb_10M','graph_paper'];
-algorithm=['MRG','ARG','MRO','ARO'];
+graph=['graph-30','graph-37','graph_imdb','graph_paper']
+algorithm=['MRG','ARG','GRF','MRO','ARO'];
 
-sheet.write(0,0,'running time (sec)')
-for i in range(0,4):
+sheet.write(0,0,'running time (ms)')
+for i in range(0,5):
+    sheet.write(0,1+i,algorithm[i])
+
+x=0
+for g in graph:
+    x+=1
+    sheet.write(x,0,g)
+    y=0
+    for a in algorithm:
+        y+=1
+        tot=0;
+        filename=g+'_05_'+a+'_0_0_0.000001_5.txt';
+        sheet.write(x,y,find_xls(filename,3))
+
+#================================================================================#================================================================================
+sheet = xls.add_sheet('running_time(-1)',cell_overwrite_ok=True)
+
+graph=['graph-30','graph-37','graph_imdb','graph_paper']
+algorithm=['MRG','ARG','GRF','MRO','ARO'];
+
+sheet.write(0,0,'running time (ms)')
+for i in range(0,5):
     sheet.write(0,1+i,algorithm[i])
 
 x=0
